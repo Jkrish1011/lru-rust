cce3e2e9f0a00252e3b20662e4a0c7317f03723a
Jayakrishnan Ashok <jayakrishnanashok@gmail.com>
2024-09-06 14:06:12 +0530

    tested-simple-cache

---
+++ src/cache/lru_cache.rs
@@ -0,0 +1,8 @@
+use std::collections::{HashMap, VecDeque};
+use std::hash::Hash;
+/*
+
+LRU = An LRU cache evicts the least recently accessed item when the cache reaches its capacity to make room for new items.
+
+VecDeque = The VecDeque will track the order of item usage, with the most recently used items at the front and the least recently used items at the back.
+*/

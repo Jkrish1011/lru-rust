6f5adae51f55ace925eb34e9b805550abb571ccc
Jayakrishnan Ashok <jayakrishnanashok@gmail.com>
2024-09-06 13:23:08 +0530

    ignition-commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## LRU Implementation in Rust
+
+This is a LRU cache implementation using Rust
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
+// Cache generic type which implements the Eq and Hash.
+pub struct Cache<M, N> where M: Eq + Hash {
+    // Stores the cached values
+    storage: HashMap<M, N>,
+}
+
+impl<M, N> Cache<M, N> where M: Eq + Hash + Clone {
+    // Acts like the constructor
+    pub fn new() -> Self {
+        Cache {
+            storage: HashMap::new(),
+        }
+    }
+
+    // Insertion
+    pub fn set(&mut self, key: M, value: N) -> bool{
+        self.storage.insert(key, value);
+        true
+    }
+
+    // Fetch values - Returns an Option(Some or None)
+    pub fn get(&self, key: &M) -> Option<&N> {
+        self.storage.get(key)
+    }
+}
+
+fn main() {
+    let mut cache = Cache::new();
+    cache.set("Hi", "Hello");
+    // cache.set(1, "Hello World!");
+    cache.set("1", "56790");
+    // cache.set(890, 13251920);
+
+}
